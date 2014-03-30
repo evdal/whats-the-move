305a037c32187327457e44df0310bd207273ad1a
BingBingSavaje <ccneeds@gmail.com>
2014-03-30 02:52:09 -0400

    add meteor

---
+++ wtm.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to What's the Move.";
+  };
+
+  Template.hello.events({
+    'click input': function () {
+        //insert function thing
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
